2876811490f32f3843808980bf01d2d7e3d2fb55
gowthamthenarasu <58394964+gowthamthenarasu@users.noreply.github.com>
2020-02-01 18:16:13 +0530

    data science forecasting

---
+++ forcating for arima.R
@@ -0,0 +1,29 @@
+# Getting residuals 
+resid <- residuals(Predict_new)
+resid[1:10]
+windows()
+hist(1)
+windows()
+acf(resid,lag.max = 10)
+# By principal of parcimony we will consider lag - 1  as we have so 
+# many significant lags 
+# Building Autoregressive model on residuals consider lag-1 
+k <- arima(resid, order=c(1,0,0))
+
+windows()
+acf(k$residuals,lag.max = 15)
+pred_res<- predict(arima(k$residuals,order=c(1,0,0)),n.ahead = 120)
+str(pred_res)
+pred_res$pred
+acf(k$residuals)
+write.csv(trakdata,file="trakdata.csv",col.names = F,row.names = F)
+####################### Predicting new data #############################
+library(readxl)
+test_data<-read_excel(file.choose(),1) #Load Predict_new.xlsx
+View(test_data)
+test_data<-Predict_new
+pred_new<-data.frame(predict(new_model,newdata=test_data,interval = 'predict'))
+View(pred_new)
+pred_re<-pred_res$pred[1:12]
+pred_new$fit <- pred_new$fit+pred_res$pred[1:12]
+View(pred_new)
